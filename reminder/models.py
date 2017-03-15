9bf3eb28d561211b7845e8b3582280e5783c61e0
dhruvsaini <dhruv_saini@outlook.com>
2017-03-15 18:41:10 +0530

    email, phone and datetime validation
    >Either email or phone or both must be provided. There is no need to
    provide both email and phone.
    > 'phone' field only accepts valid phone no.
    > Time can not be in past.

--- reminder/models.py
+++ reminder/models.py
@@ -1,9 +1,22 @@
 from django.db import models
 
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext_lazy as _
+from django.core.validators import RegexValidator
+from datetime import datetime
+
 #Reminder Model.
 class Reminder(models.Model):
-    email = models.EmailField()
-    phone = models.CharField(max_length = 12)
+    email = models.EmailField(blank = True)
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    phone = models.CharField(max_length = 15, validators=[phone_regex], blank = True)
     message = models.TextField()
     date = models.DateField()
     time = models.TimeField()
+
+    def clean(self):
+        if not self.email and not self.phone:
+            raise ValidationError(_('Either email or Phone is required.'))
+
+        if datetime.combine(self.date, self.time) < datetime.now():
+            raise ValidationError(_('Time can not be in past.'))
--- reminder/serializers.py
+++ reminder/serializers.py
@@ -5,6 +5,12 @@ class ReminderSerializer(serializers.HyperlinkedModelSerializer):
     #'title' field for displaying only message of the reminder.
     title = serializers.HyperlinkedIdentityField(view_name = 'reminder-title')
 
+    #Validating fields.
+    def validate(self, attrs):
+        instance = Reminder(**attrs)
+        instance.clean()
+        return attrs
+
     class Meta:
         model = Reminder
         fields = ('id', 'url', 'email', 'phone', 'message', 'date', 'time', 'title',)
